b20c8bb13f4e9d948622127269c0496a2e3a9a0e
Katsuhiko Nishimra <ktns.87@gmail.com>
2015-01-17 03:40:08 +0900

    Copyright and license notice

--- lib/blaslapack_ffi.rb
+++ lib/blaslapack_ffi.rb
@@ -1,3 +1,22 @@
+=begin
+
+Copyright Katsuhiko Nishimra, 2015.
+
+This file is part of blaslapack_ffi.
+
+blaslapack_ffi is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or (at
+your option) any later version.
+
+blaslapack_ffi is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with blaslapack_ffi.  If not, see [http://www.gnu.org/licenses/].
+=end
 require "blaslapack_ffi/version"
 
 module BlasLapackFFI
