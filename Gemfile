0c4b18bda8d3333789c8470e98fee16ad216831e
Katsuhiko Nishimra <ktns.87@gmail.com>
2015-01-20 02:06:57 +0900

    Load debugger on rspec

--- Gemfile
+++ Gemfile
@@ -2,3 +2,6 @@ source 'https://rubygems.org'
 
 # Specify your gem's dependencies in blaslapack_ffi.gemspec
 gemspec
+
+gem 'pry-debugger', :platform => :mri_19
+gem 'pry-byebug', :platform => [:mri_20, :mri_21]
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,3 +2,12 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'blaslapack_ffi'
 
 require 'rspec/its'
+
+begin
+require 'pry-byebug'
+rescue LoadError
+  begin
+    require 'pry-byebug'
+  rescue LoadError
+  end
+end
